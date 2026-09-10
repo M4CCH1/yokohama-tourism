0d180cb0246702a6f44d88c80dfa8939355d4b4e
Ryo Sakuma <rsakuma1999@gmail.com>
2026-09-10 10:22:29 +0900

    UIを更新

--- static/app.js
+++ static/app.js
@@ -898,7 +898,7 @@ const NAV_HEIGHT = 65;
 
 function getPositions() {
     return [
-        window.innerHeight * 0.65 - NAV_HEIGHT, // small
+        window.innerHeight * 0.75 - NAV_HEIGHT, // small
         window.innerHeight * 0.40,              // medium
         window.innerHeight * 0.25               // large
 
